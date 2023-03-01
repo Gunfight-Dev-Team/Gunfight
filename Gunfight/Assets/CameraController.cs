990d9b4a7bf9a31bef8702c7038ba2446ef74624
bretthickman <bahickma@calpoly.edu>
2023-02-28 17:46:54 -0800

    bg

--- Gunfight/Assets/CameraController.cs
+++ Gunfight/Assets/CameraController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Mirror;
+using static UnityEngine.GraphicsBuffer;
+using TMPro;
 
 public class CameraController : NetworkBehaviour
 {
     public GameObject cameraHolder;
-    public Vector3 offset;
+
+    public Vector3 offset;    // Offset to apply to camera position
+
+    public float deadzoneRadius;
+
+    public float damping;
+
+    private Vector3 velocity = Vector3.zero;
 
     public override void OnStartAuthority()
     {
@@ -18,7 +27,25 @@ public class CameraController : NetworkBehaviour
     {
         if (SceneManager.GetActiveScene().name == "Game")
         {
-            cameraHolder.transform.position = transform.position + offset;
+
+            float distance = Vector3.Distance(cameraHolder.transform.position, transform.position);
+
+            if (distance > deadzoneRadius)
+            {
+                Vector3 smoothedPosition = Vector3.SmoothDamp(cameraHolder.transform.position, transform.position, ref velocity, damping);
+                offset = Vector3.Lerp(cameraHolder.transform.position, smoothedPosition, damping);
+                cameraHolder.transform.position = Vector3.Lerp(cameraHolder.transform.position, smoothedPosition, damping);
+            }
+            else
+            {
+                cameraHolder.transform.position = offset;
+            }
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, deadzoneRadius);
     }
 }
