8e96796f21e68f9c1a3f404935ae1d2730c7361c
bretthickman <bahickma@calpoly.edu>
2023-03-01 16:17:59 -0800

    Cam Mouse Update

--- Gunfight/Assets/CameraController.cs
+++ Gunfight/Assets/CameraController.cs
@@ -10,15 +10,21 @@ public class CameraController : NetworkBehaviour
 {
     public GameObject cameraHolder;
 
+    public Camera cam;
+
     public GameObject target;
 
     public Vector3 offset;    // Offset to apply to camera position
 
     public float deadzoneRadius;
 
+    public float mouseRangeRadius;
+
     public float damping;
 
     private Vector3 velocity = Vector3.zero;
+
+    private Vector3 trackedPosition;
 
     public override void OnStartAuthority()
     {
@@ -30,11 +36,22 @@ public class CameraController : NetworkBehaviour
         if (SceneManager.GetActiveScene().name == "Game")
         {
 
-            float distance = Vector3.Distance(cameraHolder.transform.position, target.transform.position);
+            float mouseDistance = Vector3.Distance(cameraHolder.transform.position, trackedPosition);
+
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition = cam.ScreenToWorldPoint(mousePosition);
+
+            float curRadius = mouseRangeRadius / (1 / mouseDistance);
+            
+            float subDistances = curRadius / mouseDistance;
+            trackedPosition.x = ((1.0f - subDistances)* target.transform.position.x + (subDistances*mousePosition.x));
+            trackedPosition.y = ((1.0f - subDistances) * target.transform.position.y + (subDistances * mousePosition.y));
+
+            float distance = Vector3.Distance(cameraHolder.transform.position, trackedPosition);
 
             if (distance > deadzoneRadius)
             {
-                Vector3 smoothedPosition = Vector3.SmoothDamp(cameraHolder.transform.position, target.transform.position, ref velocity, damping);
+                Vector3 smoothedPosition = Vector3.SmoothDamp(cameraHolder.transform.position, trackedPosition, ref velocity, damping);
                 cameraHolder.transform.position = smoothedPosition;
             }
         }
