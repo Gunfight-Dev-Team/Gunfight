56bfe4c0cbea29cb35e693b9ab0d49645f46d659
GelzoneXUnsas <51806064+GelzoneXUnsas@users.noreply.github.com>
2023-03-08 14:21:16 -0800

    adding camera limiter

--- Gunfight/Assets/Scripts/CameraController.cs
+++ Gunfight/Assets/Scripts/CameraController.cs
@@ -29,17 +29,35 @@ public class CameraController : NetworkBehaviour
 
     private Vector3 trackedPosition;
 
+    public BoxCollider2D boundBox;
+
+    private Vector3 minBounds;
+
+    private Vector3 maxBounds;
+
+    private float halfHeight;
+
+    private float halfWidth;
+
     public override void OnStartAuthority()
     {
         cameraHolder.SetActive(true);
+        minBounds = boundBox.bounds.min;
+        maxBounds = boundBox.bounds.max;
+        halfHeight = cam.orthographicSize;
+        halfWidth = halfHeight * Screen.width / Screen.height;
     }
 
-    public void Update()
+    public void FixedUpdate()
     {
         if (SceneManager.GetActiveScene().name == "Game")
         {
+            float clampedX = Mathf.Clamp(cameraHolder.transform.position.x, minBounds.x + halfWidth, maxBounds.x - halfWidth);
+            float clampedY = Mathf.Clamp(cameraHolder.transform.position.y, minBounds.y + halfHeight, maxBounds.y - halfHeight);
 
-            float mouseDistance = Vector3.Distance(cameraHolder.transform.position, trackedPosition);
+            Vector3 clampedPos = new Vector3(clampedX,clampedY,cameraHolder.transform.position.z);
+
+            float mouseDistance = Vector3.Distance(clampedPos, trackedPosition);
 
             Vector3 mousePosition = Input.mousePosition;
             mousePosition = cam.ScreenToWorldPoint(mousePosition);
@@ -50,11 +68,12 @@ public class CameraController : NetworkBehaviour
             trackedPosition.x = ((1.0f - subDistances)* target.transform.position.x + (subDistances*mousePosition.x));
             trackedPosition.y = ((1.0f - subDistances) * target.transform.position.y + (subDistances * mousePosition.y));
 
-            float distance = Vector3.Distance(cameraHolder.transform.position, trackedPosition);
+            float distance = Vector3.Distance(clampedPos, trackedPosition);
 
             if (distance > deadzoneRadius)
             {
-                Vector3 smoothedPosition = Vector3.SmoothDamp(cameraHolder.transform.position, trackedPosition, ref velocity, damping);
+                //Vector3 smoothedPosition = Vector3.SmoothDamp(cameraHolder.transform.position, trackedPosition, ref velocity, damping);
+                Vector3 smoothedPosition = Vector3.Lerp(clampedPos, trackedPosition, 10f*Time.deltaTime);
                 rb.MovePosition(smoothedPosition);
                 //cameraHolder.transform.position = smoothedPosition;
             }
