9b66d1b6730fc14516f9d28a52679644daa8b7bd
Ping-Wei Lin <ping-wei@Marisas-MacBook-Pro-4.local>
2023-03-08 16:23:49 -0800

    change sprite color based on team

--- Gunfight/Assets/Scripts/PlayerMovementController.cs
+++ Gunfight/Assets/Scripts/PlayerMovementController.cs
@@ -4,6 +4,14 @@ using Mirror;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+public enum Team
+{
+    Green,
+    Red,
+    Orange,
+    White
+}
+
 public class PlayerMovementController : NetworkBehaviour
 {
     public float Speed = 5.0f;
@@ -15,6 +23,15 @@ public class PlayerMovementController : NetworkBehaviour
     public Camera cam;
 
     public PlayerObjectController poc;
+
+    [SerializeField] public Team team;
+
+    [SerializeField] public Sprite[] greenSprite;
+    [SerializeField] public Sprite[] redSprite;
+    [SerializeField] public Sprite[] orangeSprite;
+    [SerializeField] public Sprite[] whiteSprite;
+    public GameObject player;
+    public SpriteRenderer spriteRenderer;
 
     private Vector2 mousePos;
 
@@ -31,6 +48,7 @@ public class PlayerMovementController : NetworkBehaviour
             if (PlayerModel.activeSelf == false)
             {
                 SetPosition();
+                SetTeam();
                 PlayerModel.SetActive(true);
             }
 
@@ -38,6 +56,69 @@ public class PlayerMovementController : NetworkBehaviour
             {
                 Movement();
             }
+        }
+    }
+
+    private void Update()
+    {
+        if (team.Equals(Team.Green))
+        {
+            // [ ] TODO: change it to be using event intead of update
+            if (player.GetComponent<PlayerInfo>().weaponID.Equals(WeaponID.AK47))
+                spriteRenderer.sprite = greenSprite[0];
+            else if (player.GetComponent<PlayerInfo>().weaponID.Equals(WeaponID.Knife))
+                spriteRenderer.sprite = greenSprite[1];
+            else if (player.GetComponent<PlayerInfo>().weaponID.Equals(WeaponID.Pistol))
+                spriteRenderer.sprite = greenSprite[2];
+            else if (player.GetComponent<PlayerInfo>().weaponID.Equals(WeaponID.Sniper))
+                spriteRenderer.sprite = greenSprite[3];
+            else if (player.GetComponent<PlayerInfo>().weaponID.Equals(WeaponID.Uzi))
+                spriteRenderer.sprite = greenSprite[4];
+        }
+
+        if (team.Equals(Team.Red))
+        {
+            // [ ] TODO: change it to be using event intead of update
+            if (player.GetComponent<PlayerInfo>().weaponID.Equals(WeaponID.AK47))
+                spriteRenderer.sprite = redSprite[0];
+            else if (player.GetComponent<PlayerInfo>().weaponID.Equals(WeaponID.Knife))
+                spriteRenderer.sprite = redSprite[1];
+            else if (player.GetComponent<PlayerInfo>().weaponID.Equals(WeaponID.Pistol))
+                spriteRenderer.sprite = redSprite[2];
+            else if (player.GetComponent<PlayerInfo>().weaponID.Equals(WeaponID.Sniper))
+                spriteRenderer.sprite = redSprite[3];
+            else if (player.GetComponent<PlayerInfo>().weaponID.Equals(WeaponID.Uzi))
+                spriteRenderer.sprite = redSprite[4];
+        }
+
+        if (team.Equals(Team.Orange))
+        {
+            // [ ] TODO: change it to be using event intead of update
+            if (player.GetComponent<PlayerInfo>().weaponID.Equals(WeaponID.AK47))
+                spriteRenderer.sprite = orangeSprite[0];
+            else if (player.GetComponent<PlayerInfo>().weaponID.Equals(WeaponID.Knife))
+                spriteRenderer.sprite = orangeSprite[1];
+            else if (player.GetComponent<PlayerInfo>().weaponID.Equals(WeaponID.Pistol))
+                spriteRenderer.sprite = orangeSprite[2];
+            else if (player.GetComponent<PlayerInfo>().weaponID.Equals(WeaponID.Sniper))
+                spriteRenderer.sprite = orangeSprite[3];
+            else if (player.GetComponent<PlayerInfo>().weaponID.Equals(WeaponID.Uzi))
+                spriteRenderer.sprite = orangeSprite[4];
+        }
+
+        if (team.Equals(Team.White))
+        {
+            // [ ] TODO: change it to be using event intead of update
+            if (player.GetComponent<PlayerInfo>().weaponID.Equals(WeaponID.AK47))
+                spriteRenderer.sprite = whiteSprite[0];
+            else if (player.GetComponent<PlayerInfo>().weaponID.Equals(WeaponID.Knife))
+                spriteRenderer.sprite = whiteSprite[1];
+            else if (player.GetComponent<PlayerInfo>().weaponID.Equals(WeaponID.Pistol))
+                spriteRenderer.sprite = whiteSprite[2];
+            else if (player.GetComponent<PlayerInfo>().weaponID.Equals(WeaponID.Sniper))
+                spriteRenderer.sprite = whiteSprite[3];
+            else if (player.GetComponent<PlayerInfo>().weaponID.Equals(WeaponID.Uzi))
+                spriteRenderer.sprite = whiteSprite[4];
         }
     }
 
@@ -51,6 +132,30 @@ public class PlayerMovementController : NetworkBehaviour
             PlayerModel.transform.position = new Vector3(-22.5f, 22.5f, 0.0f);
         if (poc.PlayerIdNumber == 4)
             PlayerModel.transform.position = new Vector3(22.5f, -22.5f, 0.0f);
+    }
+
+    public void SetTeam()
+    {
+        if (poc.PlayerIdNumber == 1)
+        {
+            team = Team.Green;
+            spriteRenderer.sprite = greenSprite[1];
+        }
+        else if (poc.PlayerIdNumber == 2)
+        {
+            team = Team.Red;
+            spriteRenderer.sprite = redSprite[1];
+        }
+        else if (poc.PlayerIdNumber == 3)
+        {
+            team = Team.Orange;
+            spriteRenderer.sprite = orangeSprite[1];
+        }
+        else if (poc.PlayerIdNumber == 4)
+        {
+            team = Team.White;
+            spriteRenderer.sprite = whiteSprite[1];
+        }
     }
 
     public void Movement()
--- Gunfight/Assets/Scripts/Weapon/WeaponController.cs
+++ Gunfight/Assets/Scripts/Weapon/WeaponController.cs
@@ -2,85 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace MyPlayer
+// If time permit, take a look at Object Pool
+public class WeaponController : MonoBehaviour
 {
-    // If time permit, take a look at Object Pool
-    public class WeaponController : MonoBehaviour
+    [SerializeField] private GameObject AK47;
+    [SerializeField] private GameObject Knife;
+    [SerializeField] private GameObject Pistol;
+    [SerializeField] private GameObject Sniper;
+    [SerializeField] private GameObject Uzi;
+
+    public WeaponInfo weapon;
+    public GameObject Player;
+
+    private bool canPickup = false;
+
+    private Collider2D OtherCollider;
+
+    void OnTriggerEnter2D(Collider2D other)
     {
-        [SerializeField] private GameObject AK47;
-        [SerializeField] private GameObject Knife;
-        [SerializeField] private GameObject Pistol;
-        [SerializeField] private GameObject Sniper;
-        [SerializeField] private GameObject Uzi;
-
-        public WeaponInfo weapon;
-        public GameObject Player;
-
-        private bool canPickup = false;
-
-        private Transform child;
-
-        private Collider2D OtherCollider;
-
-        private void Start()
+        if (other.CompareTag("Player"))
         {
-            child = Player.transform.Find("Player");
+            canPickup = true;
+            OtherCollider = other;
         }
+    }
 
-        void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
         {
-            if (other.CompareTag("Player"))
-            {
-                canPickup = true;
-                OtherCollider = other;
-            }
+            canPickup = false;
         }
+    }
 
-        void OnTriggerExit2D(Collider2D other)
+    void Update()
+    {
+        if (canPickup && Input.GetKeyDown(KeyCode.E))
         {
-            if (other.CompareTag("Player"))
-            {
-                canPickup = false;
-            }
+            // Pick up the weapon
+            Debug.Log("Weapon picked up!");
+            Drop(OtherCollider.GetComponent<PlayerInfo>().weaponID, OtherCollider);
+            PickUp(OtherCollider);
         }
+    }
 
-        void Update()
-        {
-            if (canPickup && Input.GetKeyDown(KeyCode.E))
-            {
-                // Pick up the weapon
-                Debug.Log("Weapon picked up!");
-                Drop(OtherCollider.GetComponent<PlayerInfo>().weaponID, OtherCollider);
-                PickUp(OtherCollider);
-            }
-        }
+    void Drop(WeaponID newWeaponID, Collider2D other)
+    {
+        // [ ] TODO: is it possible to make this more simple?
+        var weapons = new Dictionary<WeaponID, GameObject>(){
+            {WeaponID.AK47, AK47},
+            {WeaponID.Knife, Knife},
+            {WeaponID.Pistol, Pistol},
+            {WeaponID.Sniper, Sniper},
+            {WeaponID.Uzi, Uzi}
+        };
 
-        void Drop(WeaponID newWeaponID, Collider2D other)
-        {
-            // [ ] TODO: is it possible to make this more simple?
-            var weapons = new Dictionary<WeaponID, GameObject>(){
-                {WeaponID.AK47, AK47},
-                {WeaponID.Knife, Knife},
-                {WeaponID.Pistol, Pistol},
-                {WeaponID.Sniper, Sniper},
-                {WeaponID.Uzi, Uzi}
-            };
+        GameObject newWeapon = Instantiate(weapons[newWeaponID], 
+                                    OtherCollider.GetComponent<Transform>().position, 
+                                    weapons[newWeaponID].transform.rotation);
+        newWeapon.GetComponent<WeaponInfo>().nAmmo = other.GetComponent<PlayerInfo>().nAmmo;
+        newWeapon.GetComponent<WeaponInfo>().range = other.GetComponent<PlayerInfo>().range;
+        newWeapon.GetComponent<WeaponInfo>().speedOfPlayer = other.GetComponent<PlayerInfo>().speedOfPlayer;
+    }
 
-            GameObject newWeapon = Instantiate(weapons[newWeaponID], 
-                                        OtherCollider.GetComponent<Transform>().position, 
-                                        weapons[newWeaponID].transform.rotation);
-            newWeapon.GetComponent<WeaponInfo>().nAmmo = other.GetComponent<PlayerInfo>().nAmmo;
-            newWeapon.GetComponent<WeaponInfo>().range = other.GetComponent<PlayerInfo>().range;
-            newWeapon.GetComponent<WeaponInfo>().speedOfPlayer = other.GetComponent<PlayerInfo>().speedOfPlayer;
-        }
-
-        void PickUp(Collider2D other)
-        {
-            other.GetComponent<PlayerInfo>().weaponID = weapon.id;
-            other.GetComponent<PlayerInfo>().nAmmo = weapon.nAmmo;
-            other.GetComponent<PlayerInfo>().range = weapon.range;
-            other.GetComponent<PlayerInfo>().speedOfPlayer = weapon.speedOfPlayer;
-            Destroy(gameObject);
-        }
+    void PickUp(Collider2D other)
+    {
+        other.GetComponent<PlayerInfo>().weaponID = weapon.id;
+        other.GetComponent<PlayerInfo>().nAmmo = weapon.nAmmo;
+        other.GetComponent<PlayerInfo>().range = weapon.range;
+        other.GetComponent<PlayerInfo>().speedOfPlayer = weapon.speedOfPlayer;
+        Destroy(gameObject);
     }
 }
