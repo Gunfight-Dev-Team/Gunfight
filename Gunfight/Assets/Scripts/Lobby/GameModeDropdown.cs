7730c04a4c3060789772438d3427368df44cce98
bretthickman <bahickma@calpoly.edu>
2023-10-26 15:24:58 -0700

    Figuring Out Rpc Stuff

--- Gunfight/Assets/Scripts/Lobby/GameModeDropdown.cs
+++ Gunfight/Assets/Scripts/Lobby/GameModeDropdown.cs
@@ -10,23 +10,15 @@ public class GameModeDropdown : NetworkBehaviour
 
     public void OnDropdownValueChanged()
     {
-        Debug.Log("1");
         // Call a command to change the Dropdown value on the server
-        CmdChangeDropdownValue(dropdown.value);
-    }
-
-    [Command(requiresAuthority = false)]
-    void CmdChangeDropdownValue(int value)
-    {
-        Debug.Log("2");
-        // Change the Dropdown value on the server
-        RpcSyncDropdownValue(value);
+        if(isServer)
+            RpcSyncDropdownValue(dropdown.value);
     }
 
     [ClientRpc]
     void RpcSyncDropdownValue(int value)
     {
-        Debug.Log("3");
+        Debug.Log("2");
         // Change the Dropdown value on all clients
         dropdown.value = value;
     }
