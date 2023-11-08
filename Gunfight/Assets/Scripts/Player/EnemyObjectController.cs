a65c6d0b4ad95855ade47fd3270a19ed6635bc3a
bretthickman <gelzonexunsas@gmail.com>
2023-11-08 15:50:04 -0800

    init emeny

---
+++ Gunfight/Assets/Scripts/Player/EnemyObjectController.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using Steamworks;
+using UnityEngine.SceneManagement;
+
+public class EnemyObjectController : NetworkBehaviour
+{
+    [SyncVar(hook = nameof(RoundUpdate))] public float speed = 1f;
+    public float speedOffset = 0.1f;
+    public GameObject closestPlayer;
+    private float closestDistance = float.MaxValue;
+
+
+    private GameObject[] players;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        rb = GetComponent<Rigidbody2D>();
+        closestPlayer = GameObject.FindGameObjectWithTag("Player");
+        InvokeRepeating("FindClosest", 0f, 3f);
+    }
+
+    void Update()
+    {
+        transform.up = (closestPlayer.transform.position - transform.position).normalized;
+
+        rb.MovePosition(transform.position + transform.up * speed * Time.deltaTime);
+        Physics2D.SyncTransforms();
+
+    }
+
+    private void FindClosest()
+    {
+
+        foreach (GameObject player in players)
+        {
+            float distance = Vector3.Distance(transform.position, player.transform.position);
+
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestPlayer = player;
+            }
+        }
+
+    }
+
+    public void RoundUpdate(float OldValue, float NewValue)
+    {
+        // TODO: if round ends, increase speed
+        if (isServer)
+        {
+            speed = NewValue;
+        }
+    }
+
+    [ClientRpc]
+    private void increaseSpeed(float OldValue)
+    {
+        float newSpeed = OldValue * (1 + Random.Range(-speedOffset, speedOffset));
+        RoundUpdate(OldValue, newSpeed);
+    }
+}
