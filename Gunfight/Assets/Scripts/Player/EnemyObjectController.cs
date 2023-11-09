1df3e598c8e1c140673095c31c2ed8f9122e4f2d
Lucas Li <gelzonexunsas@gmail.com>
2023-11-09 00:57:49 -0800

    in progress speed

--- Gunfight/Assets/Scripts/Player/EnemyObjectController.cs
+++ Gunfight/Assets/Scripts/Player/EnemyObjectController.cs
@@ -8,50 +8,48 @@ using UnityEngine.SceneManagement;
 public class EnemyObjectController : NetworkBehaviour
 {
     public Pathfinding.AIDestinationSetter target;
+    public Pathfinding.AIPath path;
     public SpriteRenderer spriteRenderer;
 
-    [SyncVar(hook = nameof(RoundUpdate))] public float speed = 1f;
+    [SyncVar(hook = nameof(RoundUpdate))] public float speed;
     public float speedOffset = 0.1f;
     public GameObject closestPlayer;
-    //private float closestDistance = float.MaxValue;
+    private float closestDistance = float.MaxValue;
 
-    //private GameObject[] players;
-    //private Rigidbody2D rb;
+    private GameObject[] players;
 
     private Vector3 previousPosition;
 
     void Start()
     {
-        //players = GameObject.FindGameObjectsWithTag("Player");
-        //rb = GetComponent<Rigidbody2D>();
-        //closestPlayer = GameObject.FindGameObjectWithTag("Player");
-        target.target = GameObject.FindGameObjectWithTag("Player").transform;
-        //InvokeRepeating("FindClosest", 0f, 3f);
 
+        target.target = GameObject.FindGameObjectWithTag("Player").transform;
+        InvokeRepeating("FindClosest", 0f, 3f);
+        InvokeRepeating("increaseSpeed", 0f, 3f);
+        path.maxSpeed *= speed;
         previousPosition = transform.position;
     }
 
     void Update()
     {
         updateFlip();
-
     }
 
-    /*    private void FindClosest()
+    private void FindClosest()
+    {
+        
+        foreach (GameObject player in players)
         {
-
-            foreach (GameObject player in players)
+            float distance = Vector3.Distance(transform.position, player.transform.position);
+            
+            if (distance < closestDistance)
             {
-                float distance = Vector3.Distance(transform.position, player.transform.position);
-
-                if (distance < closestDistance)
-                {
-                    closestDistance = distance;
-                    closestPlayer = player;
-                }
+                closestDistance = distance;
+                target.target = player.transform;
             }
+        }
 
-        }*/
+    }
 
     public void RoundUpdate(float OldValue, float NewValue)
     {
@@ -67,6 +65,7 @@ public class EnemyObjectController : NetworkBehaviour
     {
         float newSpeed = OldValue * (1 + speedOffset);
         RoundUpdate(OldValue, newSpeed);
+        path.maxSpeed *= speed;
     }
 
     void updateFlip()
