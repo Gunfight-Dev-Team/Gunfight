76e66c4ef1002cc4dff6a2c880bd448a96c8ab85
akfanelli <86266606+akfanelli@users.noreply.github.com>
2024-01-04 18:44:35 -0800

    winning card display longer

--- Gunfight/Assets/Scripts/GameModeManager.cs
+++ Gunfight/Assets/Scripts/GameModeManager.cs
@@ -275,7 +275,7 @@ public class GameModeManager : NetworkBehaviour
                     }
                     winningCard = cardManager.FindWinningCard();
                     RpcShowWinningCard(winningCard);
-                    yield return new WaitForSeconds(2f);
+                    yield return new WaitForSeconds(5f);
                     RpcStopShowWinner();
                     RpcStopCardPanel();
                     StartCoroutine(Countdown());
