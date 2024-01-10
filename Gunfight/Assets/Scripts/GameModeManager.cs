9dfb29ade55bdf0bef2be420500f675b9686253b
akfanelli <akfanell@calpoly.edu>
2024-01-09 21:12:16 -0800

    disable player test 2

--- Gunfight/Assets/Scripts/GameModeManager.cs
+++ Gunfight/Assets/Scripts/GameModeManager.cs
@@ -426,7 +426,7 @@ public class GameModeManager : NetworkBehaviour
         // Call the disable game interaction for all players
         foreach (PlayerObjectController player in Manager.GamePlayers)
         {
-            player.GetComponent<PlayerController>().SetActive(false);
+            player.enabled = false;
         }
     }
 
@@ -437,7 +437,7 @@ public class GameModeManager : NetworkBehaviour
         // Call the reset function for all players
         foreach (PlayerObjectController player in Manager.GamePlayers)
         {
-            player.GetComponent<PlayerController>().SetActive(true);
+            player.enabled = true;
             player.GetComponent<PlayerController>().Respawn();
             player.isAlive = true;
         }
